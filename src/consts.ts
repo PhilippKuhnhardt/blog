8e46e8aab1e8a8686761764ac71c4ddc157f5ae7
Philipp Kuhnhardt <p.kuhnhardt@gmx.de>
2026-05-04 20:51:02 +0200

    feat: final content adjustments

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
   NAME: "Philipp's Blog",
-  EMAIL: "tbd@mail.de",
+  EMAIL: "philipps.blog@posteo.de",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
